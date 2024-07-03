13555af0940af924994c26ad74d6a0763f3a4582
pirunthavansarma <pirunthavansarma@gmail.com>
2024-07-03 16:51:59 +0530

    first commit

---
+++ java.js
@@ -0,0 +1,13 @@
+document.getElementById('contact-button').addEventListener('click',()=>{
+    document.getElementById('modal').style.display="block";
+
+})
+
+document.getElementById('close').addEventListener('click',()=>{
+    document.getElementById("model").style.display="none"
+})
+
+document.getElementById('close').addEventListener('click',()=>{
+    document.getElementById("model").style.display="none"
+})
+
